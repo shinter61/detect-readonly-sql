d340c05f74005517752c9bb332fb78bb1c5fee84
shinter61 <shintaro-matsumoto@c-fo.com>
2023-12-05 15:42:22 +0900

    Output spec sql log

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 Rails.logger = Logger.new(STDOUT)
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
