4deca5984c4b43132ade00275027fb7cca95d3c5
shinter61 <shintaro-matsumoto@c-fo.com>
2023-12-05 15:40:17 +0900

    Run request spec

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RailsDbTuning
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.hosts << '.example.com'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
